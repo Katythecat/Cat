08746be10b596e273a2c780aa1b60b078bae4fd2
Katythecat <kk.wilasinee@gmail.com>
2023-04-03 07:52:47 -0700

    Strat the new project on 4/3/2023

---
+++ src/Kitten.java
@@ -0,0 +1,3 @@
+public class Kitten {
+    String name, age, color;
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Kitten kt=new Kitten();
+        kt.name="Avocado";
+        kt.age="3 mo";
+        kt.color="green";
+    }
+}
